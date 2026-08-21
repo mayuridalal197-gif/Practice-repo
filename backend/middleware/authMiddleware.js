a74ef0aacf79fe43ea0992cf718cbb40b89b50aa
mayuridalal197-gif <mayuridalal197@gmail.com>
2026-08-21 19:06:49 +0530

    Create authMiddleware.js
    Added JWT authentication middleware to verify access tokens and protect API routes.

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,51 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+    try {
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader) {
+            return res.status(401).json({
+                message: "Access token required"
+            });
+        }
+
+        const token = authHeader.split(" ")[1];
+
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+
+        req.user = decoded;
+
+        next();
+
+    } catch (error) {
+        res.status(401).json({
+            message: "Invalid or expired token"
+        });
+    }
+};
+
+// Role checking
+const roleMiddleware = (...allowedRoles) => {
+    return (req, res, next) => {
+
+        if (!req.user) {
+            return res.status(401).json({
+                message: "Authentication required"
+            });
+        }
+
+        if (!allowedRoles.includes(req.user.role)) {
+            return res.status(403).json({
+                message: "Access denied"
+            });
+        }
+
+        next();
+    };
+};
+
+module.exports = authMiddleware,roleMiddleware;
