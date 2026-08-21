c978f5b61e0279103ed895de5a370480b64c0698
mayuridalal197-gif <mayuridalal197@gmail.com>
2026-08-21 18:55:40 +0530

    Update appointmentRoutes.js
    Added API routes for creating, viewing, updating, and deleting appointments.

--- backend/apiRoutes/appointmentRoutes.js
+++ backend/apiRoutes/appointmentRoutes.js
@@ -1,1 +1,64 @@
+const express = require("express");
 
+const router = express.Router();
+
+const {
+    createAppointment,
+    getAppointments,
+    getAppointmentById,
+    updateAppointment,
+    deleteAppointment
+} = require("../apicontrollors/appointmentController");
+
+const {
+    authMiddleware,
+    roleMiddleware
+} = require("../middleware/authMiddleware");
+
+
+// Admin can create appointment
+router.post(
+    "/",
+    authMiddleware,
+    roleMiddleware("admin"),
+    createAppointment
+);
+
+
+// Admin can view all appointments
+router.get(
+    "/",
+    authMiddleware,
+    roleMiddleware("admin"),
+    getAppointments
+);
+
+
+// Admin can view appointment by ID
+router.get(
+    "/:id",
+    authMiddleware,
+    roleMiddleware("admin"),
+    getAppointmentById
+);
+
+
+// Admin can update appointment
+router.put(
+    "/:id",
+    authMiddleware,
+    roleMiddleware("admin"),
+    updateAppointment
+);
+
+
+// Admin can delete appointment
+router.delete(
+    "/:id",
+    authMiddleware,
+    roleMiddleware("admin"),
+    deleteAppointment
+);
+
+
+module.exports = router;
