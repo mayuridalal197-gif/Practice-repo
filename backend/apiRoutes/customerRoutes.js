0824e5625642e6af135e7ebe99fa25a48a123b36
mayuridalal197-gif <mayuridalal197@gmail.com>
2026-08-21 18:56:49 +0530

    Update customerRoutes.js
    Added API routes for creating, viewing, updating, and deleting customers.

--- backend/apiRoutes/customerRoutes.js
+++ backend/apiRoutes/customerRoutes.js
@@ -1,1 +1,64 @@
+const express = require("express");
 
+const router = express.Router();
+
+const {
+    createCustomer,
+    getCustomers,
+    getCustomersById,
+    updatecustomer,
+    deletecustomer
+} = require("../apicontrollors/customerController");
+
+const {
+    authMiddleware,
+    roleMiddleware
+} = require("../middleware/authMiddleware");
+
+
+// Admin can view all customers
+router.get(
+    "/",
+    authMiddleware,
+    roleMiddleware("admin"),
+    getCustomers
+);
+
+
+// Admin can view customer by ID
+router.get(
+    "/:id",
+    authMiddleware,
+    roleMiddleware("admin"),
+    getCustomersById
+);
+
+
+// Admin can create customer
+router.post(
+    "/",
+    authMiddleware,
+    roleMiddleware("admin"),
+    createCustomer
+);
+
+
+// Admin can update customer
+router.put(
+    "/:id",
+    authMiddleware,
+    roleMiddleware("admin"),
+    updatecustomer
+);
+
+
+// Admin can delete customer
+router.delete(
+    "/:id",
+    authMiddleware,
+    roleMiddleware("admin"),
+    deletecustomer
+);
+
+
+module.exports = router;
