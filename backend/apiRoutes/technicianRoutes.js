321da74264a0049fc339c1015a27ff4fc13f9bc8
mayuridalal197-gif <mayuridalal197@gmail.com>
2026-08-21 18:58:43 +0530

    Update technicianRoutes.js
    Added API routes for creating, viewing, updating, and deleting technicians.

--- backend/apiRoutes/technicianRoutes.js
+++ backend/apiRoutes/technicianRoutes.js
@@ -1,1 +1,64 @@
+const express = require("express");
 
+const router = express.Router();
+
+const {
+    createTechnician,
+    getTechnicians,
+    getTechnicianById,
+    updateTechnician,
+    deleteTechnician
+} = require("../apicontrollors/technicianController");
+
+const {
+    authMiddleware,
+    roleMiddleware
+} = require("../middleware/authMiddleware");
+
+
+// Admin can create technician
+router.post(
+    "/",
+    authMiddleware,
+    roleMiddleware("admin"),
+    createTechnician
+);
+
+
+// Admin can view all technicians
+router.get(
+    "/",
+    authMiddleware,
+    roleMiddleware("admin"),
+    getTechnicians
+);
+
+
+// Admin can view technician by ID
+router.get(
+    "/:id",
+    authMiddleware,
+    roleMiddleware("admin"),
+    getTechnicianById
+);
+
+
+// Admin can update technician
+router.put(
+    "/:id",
+    authMiddleware,
+    roleMiddleware("admin"),
+    updateTechnician
+);
+
+
+// Admin can delete technician
+router.delete(
+    "/:id",
+    authMiddleware,
+    roleMiddleware("admin"),
+    deleteTechnician
+);
+
+
+module.exports = router;
